7d0436de3b60da1f8e06f495b5a17f8861e41fc6
wanderson-rpf <wandersonrpf@gmail.com>
2022-08-11 19:29:29 -0300

    Adicionando item 18

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const botaoEntrar = document.getElementById('btn-entrar');
 const login = document.getElementById('input-email');
 const senha = document.getElementById('input-senha');
+const confirmacao = document.getElementById('agreement');
+const botaoEnviar = document.getElementById('submit-btn');
 
 botaoEntrar.addEventListener('click', () => {
   if (login.value !== 'tryber@teste.com' && senha.value !== 123456) {
     alert('Email ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
+  }
+});
+
+confirmacao.addEventListener('click', () => {
+  if (confirmacao.checked) {
+    botaoEnviar.disabled = false;
+  } else {
+    botaoEnviar.disabled = true;
   }
 });
